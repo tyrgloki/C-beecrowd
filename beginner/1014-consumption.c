9781b65d5b8b39111dc9ede824543b398c2b4623
Loki <riqc0nt4ct@hotmail.com>
2022-06-18 16:24:25 -0300

    1014-consumption

---
+++ beginner/1014-consumption.c
@@ -0,0 +1,19 @@
+// Calculate a car's average consumption being provided the total distance traveled (in Km) 
+// and the spent fuel total (in liters).
+
+#include <stdio.h>
+
+int main() {
+
+    int X;
+    float Y, consumption;
+
+    scanf("%d", &X);
+    scanf("%f", &Y);
+
+    consumption = X / Y;
+
+    printf("%.3f km/l\n", consumption);
+
+    return 0;
+}
