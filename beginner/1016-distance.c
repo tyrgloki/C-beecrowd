518157a42b963d0f25d62aa3eaec8e79161024fa
Loki <riqc0nt4ct@hotmail.com>
2022-06-22 17:00:39 -0300

    beginner/1016-distance

---
+++ beginner/1016-distance.c
@@ -0,0 +1,19 @@
+// Two cars (X and Y) leave in the same direction. The car X leaves with a constant speed of 
+// 60 km/h and the car Y leaves with a constant speed of 90 km / h.
+// In one hour (60 minutes) the car Y can get a distance of 30 kilometers from the X car, 
+// in other words, it can get away one kilometer for each 2 minutes.
+// Read the distance (in km) and calculate how long it takes (in minutes) for the car Y 
+// to take this distance in relation to the other car.
+
+#include <stdio.h>
+
+int main() {
+
+    int km;
+
+    scanf("%d", &km);
+    
+    printf("%d minutos\n", km * 2);
+
+    return 0;
+}
